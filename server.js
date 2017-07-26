c0f17cd0e797de2ca4dc05fe80d0b0a9d271bb3e
mosherachamim <info@web4you.co.il>
2017-07-26 03:12:28 +0300

    u

--- server.js
+++ server.js
@@ -3,15 +3,15 @@
 var SQLMAXCONNECTIONS = 90;
 var SERVER_PORT = 1337;
 //dev
-//var SQL_URL = "212.179.232.90";
-//var SQL_User = "sa";
-//var SQL_Password ="123456";
-//var SQL_DB_Name =  "moshe";
+var SQL_URL = "212.179.232.90";
+var SQL_User = "sa";
+var SQL_Password ="123456";
+var SQL_DB_Name =  "moshe";
 //prod
-var SQL_URL = "81.218.117.73";
-var SQL_User = "wiselyev_wiselys";
-var SQL_Password = "KT{r#fI&fv9c";
-var SQL_DB_Name = "wiselyev_wisely_app_sit";
+//var SQL_URL = "81.218.117.73";
+//var SQL_User = "wiselyev_wiselys";
+//var SQL_Password = "KT{r#fI&fv9c";
+//var SQL_DB_Name = "wiselyev_wisely_app_sit";
 
 var express = require('express');
 var app = express();
